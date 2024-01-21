bb63a8a962ce8691e4c846cf5aa63253890851f0
tomatNasheDelo <tomat.nashe.delo@gmail.com>
2024-01-21 21:28:50 +0300

    metod dobavlen v otdelnuyu direktoriyu

--- src/main/java/com/example/demo/ByteDemoApplication.java
+++ src/main/java/com/example/demo/ByteDemoApplication.java
@@ -34,25 +34,25 @@ public class ByteDemoApplication {
 //              
 //            System.out.println(ex.getMessage());
   //        }
-		
-		 String searchWord = "Hello"; // слово заменить на нужное
-         FileInputStream fis = new FileInputStream("notes.txt"); // путь заменить на нужный
-         byte[] content = new byte[fis.available()];
-         fis.read(content);
-         fis.close();
-        String[] lines = new String(content, "Cp1251").split("\n"); // кодировку указать нужную
-         int i = 1;
-        for (String line : lines) {
-             String[] words = line.split(" ");
-             int j = 1;
-             for (String word : words) {
-                 if (word.equalsIgnoreCase(searchWord)) {
-                     System.out.println("Найдено в " + i + "-й строке, " + j + "-е слово");
-                 }
-                 j++;
-            }
-              i++;
-        }
+//		
+//		 String searchWord = "Hello"; // слово заменить на нужное
+//         FileInputStream fis = new FileInputStream("notes.txt"); // путь заменить на нужный
+//         byte[] content = new byte[fis.available()];
+//         fis.read(content);
+//         fis.close();
+//        String[] lines = new String(content, "Cp1251").split("\n"); // кодировку указать нужную
+//         int i = 1;
+//        for (String line : lines) {
+//             String[] words = line.split(" ");
+//             int j = 1;
+//             for (String word : words) {
+//                 if (word.equalsIgnoreCase(searchWord)) {
+//                     System.out.println("Найдено в " + i + "-й строке, " + j + "-е слово");
+//                 }
+//                 j++;
+//            }
+//              i++;
+//        }
     }
 
 		
--- src/main/java/com/example/demo/find_text_fromfile/Main.java
+++ src/main/java/com/example/demo/find_text_fromfile/Main.java
@@ -1,33 +1,42 @@
 package com.example.demo.find_text_fromfile;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
 //import java.io.FileInputStream;
 //import java.io.FileNotFoundException;
 //import java.io.IOException;
 //
 //
-//@Component
-//public class Main {
+@Component
+  public class Main {
 //	
-//	
-//	public void findByText() throws FileNotFoundException, IOException {
-//        String searchWord = "ходил"; // слово заменить на нужное
-//        FileInputStream fis = new FileInputStream("notes.txt"); // путь заменить на нужный
-//        byte[] content = new byte[fis.available()];
-//        fis.read(content);
-//        fis.close();
-//        String[] lines = new String(content, "Cp1251").split("\n"); // кодировку указать нужную
-//        int i = 1;
-//        for (String line : lines) {
-//            String[] words = line.split(" ");
-//            int j = 1;
-//            for (String word : words) {
-//                if (word.equalsIgnoreCase(searchWord)) {
-//                    System.out.println("Найдено в " + i + "-й строке, " + j + "-е слово");
-//                }
-//                j++;
-//            }
-//            i++;
-//        }
-//    }
-//
-//}
+ 	@Bean
+   public void findText() throws FileNotFoundException , IOException {
+	
+	   String searchWord = "Hello"; // слово заменить на нужное
+       FileInputStream fis = new FileInputStream("notes.txt"); // путь заменить на нужный
+       byte[] content = new byte[fis.available()];
+       fis.read(content);
+       fis.close();
+      String[] lines = new String(content, "Cp1251").split("\n"); // кодировку указать нужную
+       int i = 1;
+      for (String line : lines) {
+           String[] words = line.split(" ");
+           int j = 1;
+           for (String word : words) {
+               if (word.equalsIgnoreCase(searchWord)) {
+                   System.out.println("Найдено в " + i + "-й строке, " + j + "-е слово");
+               }
+               j++;
+          }
+            i++;
+      }
+	
+}
+   
+  }
